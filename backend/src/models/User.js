f793278a49c4db6cba990568c83780873869022a
Maicon Mares <maiconmaresunb@gmail.com>
2020-02-11 13:34:12 -0300

    Implementando model do usuario.

---
+++ backend/src/models/User.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+const mongoosePaginate = require('mongoose-paginate');
+
+const UserSchema = new mongoose.Schema({
+    github_username: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String, 
+        required: true,
+    },
+    avatar_url: {
+        type: String,
+        required: true,
+    },
+    techs: {
+        type: [String],
+        required: false,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    }
+});
+
+UserSchema.plugin(mongoosePaginate);
+mongoose.model('User', UserSchema);
