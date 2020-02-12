2e0d1ee8078e57cf5841064a25cad8160c4ac891
Maicon Mares <maiconmaresunb@gmail.com>
2020-02-12 16:47:51 -0300

    Alterando campo description da model User.

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -8,7 +8,7 @@ const UserSchema = new mongoose.Schema({
     },
     description: {
         type: String, 
-        required: true,
+        required: false,
     },
     avatar_url: {
         type: String,
