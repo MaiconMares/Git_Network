680bfddc4d79775e6aaa32177622c0e3539117fc
Maicon Mares <maiconmaresunb@gmail.com>
2020-02-12 16:46:29 -0300

    Adicionando CORS.

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ const mongoose = require('mongoose');
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 app.use(routes);
 
 mongoose.connect('mongodb+srv://maiconmares:qnpjeposf4580@cluster0-fcxlf.mongodb.net/test?retryWrites=true&w=majority',
