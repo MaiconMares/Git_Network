7d8b50dc68f65ce48bf0220fe8742542b8d6675f
Maicon Mares <maiconmaresunb@gmail.com>
2020-02-12 16:46:05 -0300

    Removendo erro na definicao da rota.

--- backend/routes.js
+++ backend/routes.js
@@ -2,7 +2,7 @@ const { Router } = require('express');
 const routes = Router();
 const UserController = require('./src/controllers/UserController');
 
-routes.get('/', UserController.store);
+routes.post('/', UserController.store);
 routes.get('/users', UserController.index);
 
 module.exports = routes;
